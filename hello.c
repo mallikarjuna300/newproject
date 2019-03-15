dc7733189fd55a2f601b3dae009d8d415f71a81a
mallikarjuna300 <mallikarjunreddy300@github.com>
2019-03-15 14:27:46 +0530

    second _malli commit

--- hello.c
+++ hello.c
@@ -6,6 +6,9 @@ printf("welcome hello world ; \n");
 
 printf("this is mallikarjuna reddy \n");
 
+
+printf("welcome to einfochips .. \n");
+
  return 0;
 }
 
---
+++ malli.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+printf("einfochips  ahmedabad ");
+
+printf("hello dehli captial");
+ return 0;
+}
